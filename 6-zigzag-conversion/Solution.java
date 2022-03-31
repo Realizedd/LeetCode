1b1b2c146653e837a932604bc0627fbcaaf68f86
Realizedd <TheMCman12@gmail.com>
2022-03-31 23:49:45 +0900

    Incomplete Solution to (6) Zigzag Conversion

---
+++ 6-zigzag-conversion/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    private int calculateIndex(int index, int numRows) {
+        
+    }
+
+    // Approach: Method to convert index to newIndex based on index and numRows
+    public String convert(String s, int numRows) {
+        
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String in = scanner.next();
+            System.out.println(in + ": " + solution.convert(in, 4));
+        }
+        scanner.close();
+    }
+}
