7818cd949afc63b1ee4953d8b5067a48e8a9698a
Realizedd <TheMCman12@gmail.com>
2022-04-10 23:45:34 +0900

    Solution to (6) Zigzag Conversion

--- 6-zigzag-conversion/Solution.java
+++ 6-zigzag-conversion/Solution.java
@@ -1,14 +1,44 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Solution {
 
-    private int calculateIndex(int index, int numRows) {
-        
-    }
-
-    // Approach: Method to convert index to newIndex based on index and numRows
     public String convert(String s, int numRows) {
+        int length;
+
+        if (numRows <= 1 || numRows > (length = s.length())) {
+            return s;
+        }
+
+        String[] ans = new String[numRows];
         
+        int row = 0;
+        boolean inverse = false;
+
+        for (int i = 0; i < length; i++) {
+            if (ans[row] == null) {
+                ans[row] = "";
+            }
+
+            ans[row] += s.charAt(i);
+
+            if (inverse) {
+                row--;
+
+                if (row == 0) {
+                    inverse = false;
+                }
+            } else {
+                row++;
+
+                if (row == numRows - 1) {
+                    inverse = true;
+                }
+            }
+        }
+
+        return Arrays.stream(ans).collect(Collectors.joining());
     }
 
     public static void main(String[] args) {
@@ -16,7 +46,7 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String in = scanner.next();
-            System.out.println(in + ": " + solution.convert(in, 4));
+            System.out.println(in + ": " + solution.convert(in, 2));
         }
         scanner.close();
     }
