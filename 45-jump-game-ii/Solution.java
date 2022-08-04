ce805cb936a634e26019e518189bba21701845d9
Realizedd <TheMCman12@gmail.com>
2022-08-04 23:50:19 +0900

    Incomplete Solution to (45) Jump Game II

--- 45-jump-game-ii/Solution.java
+++ 45-jump-game-ii/Solution.java
@@ -1,16 +1,29 @@
 public class Solution {
 
-    public int makeJump(int[] nums, int index, int jump, int jumps) {
-        if (index + jump > nums.length - 1) {
-            return 0;
-        } else if (index + jump == nums.length - 1) {
-            return jumps + 1;
-        } else {
-            return makeJump(nums, index + jump, nums[index], jumps + 1);
+    // READ: https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/discuss/506853/Java-A-general-greedy-solution-to-process-similar-problems
+
+    public int makeJump(int[] nums, int index, int jumps) {
+        if (index == nums.length - 1) {
+            System.out.println(jumps);
+            return jumps;
         }
+
+        int maxJump = nums[index];
+
+        if (index + maxJump >= nums.length) {
+            maxJump = nums.length - 1 - index;
+        }
+
+        int jumpsCount = makeJump(nums, index + maxJump, jumps + 1);
+
+        for (int jump = maxJump - 1; jump >= 0; jump--) {
+            jumpsCount = makeJump(nums, index + jump, jumps + 1);
+        }
+
+        return jumpsCount;
     }
 
     public int jump(int[] nums) {
-        
+        return makeJump(nums, 0, 0);
     }
 }
