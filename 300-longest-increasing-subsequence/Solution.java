474b602460ccfe521cfb233b47aacaed1cd05acb
Realizedd <TheMCman12@gmail.com>
2022-08-10 23:36:05 +0900

    Incomplete Solution to (300) Longest Increasing Subsequence

--- 300-longest-increasing-subsequence/Solution.java
+++ 300-longest-increasing-subsequence/Solution.java
@@ -1,6 +1,10 @@
 public class Solution {
 
-    public int recLen(int[] nums, int index, int last, int seq) {
+    public int recLen(int[] nums, int[] ans, int index, int last, int seq) {
+        if (ans[index] >= 0) {
+            return seq + ans[index];
+        }
+
         if (index == nums.length - 1) {
             return seq;
         }
@@ -9,19 +13,37 @@ public class Solution {
         int next = nums[index + 1];
 
         if (next > cur) {
-            return Math.max(recLen(nums, index + 1, index, seq + 1), recLen(nums, index + 1, last, seq));
+            return Math.max(recLen(nums, ans, index + 1, index + 1, seq + 1), recLen(nums, ans, index + 1, last, seq));
         } else {
-            return recLen(nums, index + 1, last, seq);
+            return recLen(nums, ans, index + 1, last, seq);
         }
     }
 
     public int lengthOfLIS(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = -1;
+        }
+
         int max = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            Math.max(max, recLen(nums, i, i, 1));
+            ans[i] = recLen(nums, ans, i, i, 1);
+            max = Math.max(max, ans[i]);
         }
 
         return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[50];
+
+        for (int i = 1; i <= 50; i++) {
+            arr[i - 1] = i;
+        }
+
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLIS(arr));
     }
 }
