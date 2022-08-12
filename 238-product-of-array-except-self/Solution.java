1520063687438f77aa308de9f80fd11261956fd1
Realizedd <TheMCman12@gmail.com>
2022-08-12 23:13:40 +0900

    Solution to (238) Product of Array Except Self

--- 238-product-of-array-except-self/Solution.java
+++ 238-product-of-array-except-self/Solution.java
@@ -2,35 +2,31 @@ public class Solution {
 
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
-        int neg = 0;
-        int zeros = 0;
+        boolean hasZero = false;
         int zeroIndex = -1;
-        int prod = 1;
+        int nonZeroProd = 1;
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
-                zeros++;
+                if (hasZero) {
+                    return ans;
+                }
+                
+                hasZero = true;
                 zeroIndex = i;
                 continue;
             }
 
-            if (nums[i] < 0) {
-                neg++;
-            }
-
-            prod *= Math.abs(nums[i]);
+            nonZeroProd *= nums[i];
         }
 
-        if (zeros > 1) {
-            return ans;
-        } else if (zeros == 1) {
-            ans[zeroIndex] = (neg % 2 == 0 ? 1 : -1) * prod;
+        if (hasZero) {
+            ans[zeroIndex] = nonZeroProd;
             return ans;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            int negs = nums[i] > 0 ? neg : (neg - 1);
-            ans[i] = (negs % 2 == 0 ? 1 : -1) * (prod / Math.abs(nums[i]));
+            ans[i] = nonZeroProd / nums[i];
         }
 
         return ans;
